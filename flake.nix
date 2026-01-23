607f2c36a573965293e030ffbc618b8cef7132f8
Eelco Dolstra <edolstra@gmail.com>
2026-01-23 16:22:11 +0100

    Switch to main branch of Determinate Nix
    Flake lock file updates:
    
    • Updated input 'nix':
        'github:DeterminateSystems/nix-src/094b65a' (2026-01-23)
      → 'github:DeterminateSystems/nix-src/370987f' (2026-01-23)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.follows = "nix/nixpkgs";
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
-    nix.url = "github:DeterminateSystems/nix-src/wasm";
+    nix.url = "github:DeterminateSystems/nix-src";
     fenix = {
       url = "https://flakehub.com/f/nix-community/fenix/0.1.*.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
